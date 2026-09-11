be3de28a972a385608b1ea8821a95dc8a22871d4
poojayadav <poojapy09876@gmail.com>
2026-09-11 18:22:50 +0530

    update MONGODB connection

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// require('dotenv').config({path:'./env'});
+import dotevn from "dotenv";
+import connectDB from "./db/db.js"
+ connectDB();
+
+ dotevn.config({path:'./env'});
+ 
+
+
+
+// import express from "express";
+// const app = express();
+
+// (async ()=>{
+//   try{
+// await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+// app.on("error",(error) => {
+//     console.log("Error:",error);
+//     throw error
+//   })
+//   app.listen(process.env.PORT,()=>{
+//     console.log`app is listening on port ${process.env.PORT}`;
+//   })
+// }catch(error){
+//     console.error("ERROR:",error)
+//     throw error
+//   }
+// })()

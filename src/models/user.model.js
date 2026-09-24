165b12d17a28461bb71f0c046daf20cba77993c3
poojayadav <poojapy09876@gmail.com>
2026-09-24 10:26:01 +0530

    Add user and video model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,8 @@
 import mongoose , {Schema} from "mongoose";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt"
-
+ 
+//user schema
 const userSchema = new Schema({
     username:{
       type:String,
@@ -44,6 +45,8 @@ const userSchema = new Schema({
     }
 },{timestamps:true})
 
+//hashing password before saving to database using bcrypt
+
 userSchema.pre("save", async function(next){
   if(!this.isModified("password"))
     return next();
@@ -54,5 +57,35 @@ userSchema.pre("save", async function(next){
 userSchema.methods.isPasswordCorrect = async function (password){
  return await bcrypt.compare(password,this.password)
 }
+
+// jwt token generation
+userSchema.methods.generationAccessToken=function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+      email:this.email,
+      username:this.username,
+      fullname:this.fullname
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expireIN:process.env.ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+userSchema.methods.generationRefreshToken=function(){
+  return jwt.sign(
+    {
+      _id:this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expireIN:process.env.REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+
 
 export const User = mongoose.model("User",userSchema);
